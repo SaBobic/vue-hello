a17f933793d3ce5f29186a5ae9187914aa5e16e0
SaBobic <fabrizioettori@gmail.com>
2022-07-11 14:37:14 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const root = new Vue(
+    {
+        el:'#root',
+        name: 'My First App',
+        data: {
+            title: 'Welcome to My First Vue.js App',
+            img: 'https://www.targetweb.it/wp-content/uploads/2019/01/vuejs.png',
+            alt: 'Vue Logo',
+        }
+    }
+);
